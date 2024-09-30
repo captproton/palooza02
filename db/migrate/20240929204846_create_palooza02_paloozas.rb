927ef5773d1cffe7db79c2f0f6e5edc1d01fa7ae
captproton <carl@wdwhub.net>
2024-09-29 17:49:57 -0700

    CHANGE: added ref to account for palooza model

--- db/migrate/20240929204846_create_palooza02_paloozas.rb
+++ db/migrate/20240929204846_create_palooza02_paloozas.rb
@@ -4,6 +4,7 @@ class CreatePalooza02Paloozas < ActiveRecord::Migration[7.1]
       t.string :title
       t.text :content
       t.datetime :published_at
+      t.references :account, null: false, foreign_key: true
 
       t.timestamps
     end
